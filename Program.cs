52d16c63ad795635cb13f38e36f4a4a14ac0531c
AlpDereli <alpdereli26@gmail.com>
2023-07-30 21:06:13 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] dizi = {23, 12, 4, 86, 72, 3,11, 17};
+foreach(var sayi in dizi)
+{
+    Console.WriteLine(sayi);
+}
+Array.Sort(dizi);
+foreach(var sayi in dizi)
+{
+    Console.WriteLine(sayi);
+}
+Array.Clear(dizi,2,2);
+foreach(var sayi in dizi)
+{
+    Console.WriteLine(sayi);
+}
+Array.Reverse(dizi);
+foreach(var sayi in dizi)
+{
+    Console.WriteLine(sayi);
+}
+Console.WriteLine(Array.IndexOf(dizi, 23));
+Array.Resize<int>(ref dizi, 9);
+dizi[8] = 99;
+foreach(var sayi in dizi)
+{
+    Console.WriteLine(sayi);
+}
